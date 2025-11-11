4ab60dc18e21ba8bb70a81143fe463b21e449eb6
noor mostafa <nour.alimohamed@student.giu-uni.de>
2025-11-11 09:42:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,42 +7,133 @@
 
 
 using namespace std;
-
-// ==================== Person Class Implementation =========================
-
+#include<iostream>;
 
 
+#include <string>
+using namespace std;
+class Person{
+private:
+    string name;
+    int id;
+    public:
+    Person() {
+        name="";
+        id=0;
+    }
+    Person(string n, int i) {
+        name = n;
+        id = i;
+    }
+     void display() {
+        cout<<"name: "<<name<<endl;
+        cout<<"id: "<<id<<endl;
+    }
+    string getName() {
+        return name;
+    }
+    ~Person() {}
+};
+
+class Student:Person {
+protected:
+    string major;
+    int yearlevel;
+public:
+    Student(){}
+    Student(string m, int l) : Person() {
+        major = m;
+        yearlevel = l;
+    }
+    Student(string n, int i,string m, int l):Person(n,i) {
+        major = m;
+        yearlevel = l;
+    }
 
 
+    void display() {
+        Person::display();
+        cout<<"major: "<<major<<endl;
+        cout<<"yearlevel: "<<yearlevel<<endl;
 
+    }
 
+    string getName(){
+        string k = Person::getName();
+        return k;
+    }
+    ~Student() {}
+};
+class Instructor:Person {
+protected:
+    string department;
+    int experienceYears;
+public:
+    Instructor() {}
+    Instructor(string n, int i, string m, int l):Person(n,i) {
+        department=m;
+        experienceYears = l;
+    }
+    void display() {
+        Person::display();
+        cout<<"department: "<<department<<endl;
+        cout<<"experience: "<<experienceYears<<endl;
+    }
+    ~Instructor() {}
+};
 
-// ==================== Student Class Implementation ====================
+//b)
 
+class Course {
+    protected:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student *students;
+    int currentStudent;
 
+    public:
+    Course( string c, string n, int m) {
+        courseCode = c;
+        courseName = n;
+        maxStudents = m;
+        currentStudent = 0;
+        this->students= new Student[m];
+    }
 
+   void addStudent(const Student& s) {
+        if (currentStudent==maxStudents) {
+            cout<<"maxStudents exceeded"<<endl;
+            return;
+        }
+        students[currentStudent] = s;
+        currentStudent++;
 
+    }
+    void display() {
+        cout<<"courseCode: "<<courseCode<<endl;
+        cout<<"courseName: "<<courseName<<endl;
+        cout<<"maxStudents: "<<maxStudents<<endl;
+        cout<<"currentStudent: "<<currentStudent<<endl;
 
+        for (int i=0;i<currentStudent;i++) {
+            cout<<"studentname : "<<students[i].getName()<<endl;
+        }
+    }
 
-// ==================== Instructor Class Implementation ====================
+    ~Course() {}
 
+};
 
-
-
-
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
+    // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-   
-    
+    Student *s1= new Student("noor",16001618,"cs",2);
+    Instructor *i1= new Instructor("yara",81,"cs",4);
+    Course *c1= new Course("101", "prog", 10);
+
+    s1->display();
+    i1->display();
+    c1->display();
+
     return 0;
-}
+};
