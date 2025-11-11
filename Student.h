68fffe9ed6840263b44846f4f410873ab1b0f932
noor mostafa <nour.alimohamed@student.giu-uni.de>
2025-11-11 09:49:58 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -7,13 +7,17 @@
 //#write student class here
 
 
-
-
-
-
-
-
-
+class Student:Person 
+private:
+    string major;
+    int yearlevel;
+public:
+    Student(){};
+    Student(string m, int l) : Person();
+    Student(string n, int i,string m, int l):Person(n,i);
+    void display();
+    string getName();
+    ~Student();
 
 
 
