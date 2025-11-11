8757b8fb90040632200ac2e5a07979842bdf53bf
noor mostafa <nour.alimohamed@student.giu-uni.de>
2025-11-11 09:48:15 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -4,7 +4,18 @@
 #include "Person.h"
 using namespace std;
 //#write Instructor class here
-
+class Student:Person {
+protected:
+    string major;
+    int yearlevel;
+public:
+    Student(){};
+    Student(string m, int l) : Person();
+    Student(string n, int i,string m, int l):Person(n,i);
+    void display();
+    string getName();
+    ~Student();
+};
 
 
 
